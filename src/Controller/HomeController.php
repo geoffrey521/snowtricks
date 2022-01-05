9154401fd3ce1e3021f6228e0a3b9bdfe84a9dd3
geoffrey521 <calvino.geoffrey@gmail.com>
2022-01-05 16:11:46 +0100

    Feature #12 : add fixtures bundle in project and tricks variable in HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(TrickRepository $repository): Response
     {
-        return $this->render('home/index.html.twig');
+        $tricks = $repository->findBy([], ['createdAt' => 'DESC'], 15, 0);
+
+        return $this->render('home/index.html.twig', [
+            'tricks' => $tricks
+        ]);
     }
 }
